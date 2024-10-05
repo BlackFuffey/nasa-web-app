6c1bb68015c2caf715982a671689d92d4923109b
rizkallahB <botrosrizkallah@gmail.com>
2024-10-05 14:35:45 -0400

    added asteroid data in AsteroidConsts

--- src/physics/PlanetConsts.js
+++ src/physics/PlanetConsts.js
@@ -1,7 +1,68 @@
 return {
     orbit: {
+        mercury: {
+            semiMajorAxis: 0.38709927,
+            eccentricity: 0.20563593,
+            inclination: 7.00497902,
+            meanLongitude: 252.25032350,
+            perihelionLongitude: 77.45779628,
+            ascendingNodeLongitude: 48.33076593,
+        },     
+        venus: {
+            semiMajorAxis: 0.72333566,
+            eccentricity: 0.00677672,
+            inclination: 3.39467605,
+            meanLongitude: 181.97909950,
+            perihelionLongitude: 131.60246718,
+            ascendingNodeLongitude: 76.67984255,
+        },          
         earth: {
-            semiMajorAxis:
-        }
-    }
+            semiMajorAxis: 1.00000261,
+            eccentricity: 0.01671123,
+            inclination: -0.00001531,
+            meanLongitude: 100.46457166,
+            perihelionLongitude: 102.93768193,
+            ascendingNodeLongitude: 0.0,
+        },
+        mars: {
+            semiMajorAxis: 1.52371034,
+            eccentricity: 0.09339410,
+            inclination: 1.84969142,
+            meanLongitude: -4.55343205,
+            perihelionLongitude: -23.94362959,
+            ascendingNodeLongitude: 49.55953891,
+        },
+        jupiter: {
+            semiMajorAxis: 5.20288700,
+            eccentricity: 0.04838624,
+            inclination: 1.30439695,
+            meanLongitude: 34.39644051,
+            perihelionLongitude: 14.72847983,
+            ascendingNodeLongitude: 100.47390909,
+        },
+        saturn: {
+            semiMajorAxis: 9.53667594,
+            eccentricity: 0.05386179,
+            inclination: 2.48599187,
+            meanLongitude: 49.95424423,
+            perihelionLongitude: 92.59887831,
+            ascendingNodeLongitude: 113.66242448,
+        },
+        uranus: {
+            semiMajorAxis: 19.18916464,
+            eccentricity: 0.04725744,
+            inclination: 0.77263783,
+            meanLongitude: 313.23810451,
+            perihelionLongitude: 170.95427630,
+            ascendingNodeLongitude: 74.01692503,
+        },
+        neptune: {
+            semiMajorAxis: 30.06992276,
+            eccentricity: 0.00859048,
+            inclination: 1.77004347,
+            meanLongitude: -55.12002969,
+            perihelionLongitude: 44.96476227,
+            ascendingNodeLongitude: 131.78422574,
+        },          
+    },
 }
