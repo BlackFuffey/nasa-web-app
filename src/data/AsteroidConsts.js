93c068516d29beca55b0a5018e1db733dfda7dfc
rizkallahB <botrosrizkallah@gmail.com>
2024-10-06 09:41:14 -0400

    added mean anomaly and period for asteroids

--- src/data/AsteroidConsts.js
+++ src/data/AsteroidConsts.js
@@ -6,6 +6,8 @@ export default {
             inclination: 10.83,
             perihelionLongitude: 178.91,
             ascendingNodeLongitude: 304.27,
+            meanAnomalyAtEpoch: 178.91,
+            period: 1.76*365.256
         },     
         albert: {
             semiMajorAxis: 2.636,
@@ -13,6 +15,8 @@ export default {
             inclination: 11.58,
             perihelionLongitude: 156.22,
             ascendingNodeLongitude: 183.86,
+            meanAnomalyAtEpoch: 156.22,
+            period: 4.28*365.256
         },          
         alinda: {
             semiMajorAxis: 2.473,
@@ -20,6 +24,8 @@ export default {
             inclination: 9.40,
             perihelionLongitude: 350.47,
             ascendingNodeLongitude: 110.42,
+            meanAnomalyAtEpoch: 350.47,
+            period: 3.89*365.256
         },
         ganymed: {
             semiMajorAxis: 2.665,
@@ -27,6 +33,8 @@ export default {
             inclination: 26.69,
             perihelionLongitude: 132.50,
             ascendingNodeLongitude: 215.49,
+            meanAnomalyAtEpoch: 132.50,
+            period: 4.35*365.256
         },
         amor: {
             semiMajorAxis: 1.92,
@@ -34,6 +42,8 @@ export default {
             inclination: 11.87,
             perihelionLongitude: 26.72,
             ascendingNodeLongitude: 171.26,
+            meanAnomalyAtEpoch: 26.72,
+            period: 2.66*356.256
         },
         icarus: {
             semiMajorAxis: 1.078,
@@ -41,6 +51,8 @@ export default {
             inclination: 22.80,
             perihelionLongitude: 31.43,
             ascendingNodeLongitude: 87.95,
+            meanAnomalyAtEpoch: 31.43,
+            period: 1.12*365.256
         },
         betulia: {
             semiMajorAxis: 2.195,
@@ -48,6 +60,8 @@ export default {
             inclination: 52.19,
             perihelionLongitude: 159.73,
             ascendingNodeLongitude: 62.23,
+            meanAnomalyAtEpoch: 159.73,
+            period: 3.25*365.256
         },
         geographos: {
             semiMajorAxis: 1.246,
@@ -55,6 +69,8 @@ export default {
             inclination: 13.34,
             perihelionLongitude: 277.01,
             ascendingNodeLongitude: 337.15,
+            meanAnomalyAtEpoch: 277.01,
+            period: 1.39*365.256
         }, 
         ivar: {
             semiMajorAxis: 1.863,
@@ -62,6 +78,8 @@ export default {
             inclination: 8.45,
             perihelionLongitude: 167.84,
             ascendingNodeLongitude: 133.08,
+            meanAnomalyAtEpoch: 167.84,
+            period: 2.54*365.256
         },  
         toro: {
             semiMajorAxis: 1.368,
@@ -69,6 +87,8 @@ export default {
             inclination: 9.38,
             perihelionLongitude: 127.27,
             ascendingNodeLongitude: 274.21,
+            meanAnomalyAtEpoch: 127.27,
+            period: 1.6*365.256
         },           
     },
 }
